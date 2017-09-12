b48bb865e52f4868f31b9de4d5ad60eaf89eec70
Graeme Rowney <rowney.graeme@googlemail.com>
2017-09-12 17:32:46 +0100

    Added Half of connection tracker

---
+++ GWareBase/Gware.Common/Networking/Connection/ConnectionTracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gware.Common.Networking.Connection
+{
+    public struct ConnectionTracker
+    {
+        public int RemoteSequence { get; private set; }
+        public uint Ack { get; private set; }
+        public IPEndPoint EndPoint { get; private set; }
+
+        public ConnectionTracker(IPEndPoint endPoint)
+        {
+            RemoteSequence = 0;
+            Ack = 0;
+            EndPoint = endPoint;
+        }
+
+        public void UpdateRemoteSequence(int remoteSequence)
+        {
+            if(remoteSequence - )
+        }
+    }
+}
