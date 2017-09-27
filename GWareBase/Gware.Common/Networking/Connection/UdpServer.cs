c0d009387a2f0a757cf9ef19b3cce8ebc0ad07c7
Graeme Rowney <rowney.graeme@googlemail.com>
2017-09-27 21:34:39 +0100

    More Progress

--- GWareBase/Gware.Common/Networking/Connection/UdpServer.cs
+++ GWareBase/Gware.Common/Networking/Connection/UdpServer.cs
@@ -12,44 +12,7 @@ namespace Gware.Common.Networking.Connection
 {
     public class UdpServer : KeyedUdpNetClient
     {
-        public UdpServer(int port, int key)
-            :base(port,key)
-        {
-
-        }
-
-        public UdpServer(int port)
-            :base(port)
-        {
-
-        }
-
-        public event SingleResultWithReturn<IPEndPoint, bool> OnClientConnected;
-        public event SingleResult<IPEndPoint, byte[]> OnDataRecevied;
-
-        protected override void OnKeyedDataReceived(IPEndPoint from, BufferReader data)
-        {
-            TransferDataPacket packet = new TransferDataPacket();
-            packet.FromBuffer(data);
-            if (packet.IsValid)
-            {
-                if (!m_connectionTrackers.ContainsKey(from))
-                {
-                    m_connectionTrackers.Add(from, new ConnectionTracker(from,OnPacketLoss));
-                }
-                m_connectionTrackers[from].UpdateRemoteSequence(packet.Header.Sequence);
-                OnValidPacketReceived(from, packet);
-            }
-
-        }
-        protected virtual void OnPacketLoss(IPEndPoint from, ushort sequence)
-        {
-
-        }
-        protected virtual void OnValidPacketReceived(IPEndPoint from, TransferDataPacket packet)
-        {
-
-        }
+        
 
         public int Broadcast(byte[] data)
         {
