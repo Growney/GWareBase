989ff61b2346e520df167f98f6747d00cd720e5a
Graeme Rowney <rowney.graeme@googlemail.com>
2018-08-11 13:14:15 +0100

    Tenant link and oauth base classes

---
+++ GWareBase/Gware.Tenancy/Configuration/IOAuthConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gware.Tenancy.Configuration
+{
+    public interface IOAuthConfiguration
+    {
+        string AuthUrl { get; }
+        string CallBackUrl { get; }
+        string ClientID { get; }
+        string Secret { get; }
+    }
+}
---
+++ GWareBase/Gware.Tenancy/Configuration/OAuthRequestArguments.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gware.Tenancy.Configuration
+{
+    public abstract class OAuthRequestArguments
+    {
+        public string RedirectPath { get; set; }
+        public string Host { get; set; }
+        public string Tenant { get; set; }
+        public string Code { get; set; }
+
+        public abstract string GetAuthenticationUrl(IOAuthConfiguration config, string state);
+        public string GetRedirectString(string state)
+        {
+            return $"http://{Host}/{RedirectPath}?state={state}";
+        }
+    }
+
+    public static class AuthRequestArgumentsExtensionMethods
+    {
+        public static T GetLocalArguments<T>(this Microsoft.AspNetCore.Http.HttpContext context, string redirectPath) 
+            where T : OAuthRequestArguments,new()
+        {
+            return new T()
+            {
+                Host = context.Request.Host.ToString(),
+                RedirectPath = redirectPath,
+                Tenant = context.Features.Get<Tenant>()?.Name
+            };
+        }
+    }
+}
